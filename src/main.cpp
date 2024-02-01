43ce24cb54de39f6ae2ab77ab85994ff7cb906ac
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-02-01 23:38:49 +0000

    Added prototypes

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,29 @@
 //******************************************************************************
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
+
+struct HammingInfo {
+  string mDNAString;
+  string mWhichStringNumber;
+  int mHammingDistance;
+};
+
+void printHeading (const string &rStr, const string &rTitle);
+void openFile (ifstream &rInFile, const string &rFileName);
+void closeFile (ifstream &rInFile);
+bool isValidDNAString (const string &rDNAString);
+int hammingDistance (const string &rStr1, const string &rStr2);
+HammingInfo hammingResults (const string &dnaInputString,
+                                const string &rDNAString,
+                                int whichStringNumber);
+HammingInfo smallestHammingDistance (const HammingInfo &rHammingInfo1,
+                                        const HammingInfo &rHammingInfo2);
+void displayHammingResults (const string &rDNAString, 
+                            const HammingInfo &rsHammingInfo);
 
 int main () {
 
