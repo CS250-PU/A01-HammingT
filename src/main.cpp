fb3ba2507ff7b41ab100490749aae5ac30f75d4e
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-02-02 18:51:58 +0000

    Added new function

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,14 @@ struct HammingInfo {
 void printHeading (const string &rStr, const string &rTitle);
 void openFile (ifstream &rInFile, const string &rFileName);
 void closeFile (ifstream &rInFile);
+string readDNAString (istream &rInFile, const string &rMessage);
 bool isValidDNAString (const string &rDNAString);
 int hammingDistance (const string &rStr1, const string &rStr2);
 HammingInfo hammingResults (const string &dnaInputString,
                                 const string &rDNAString,
                                 int whichStringNumber);
-HammingInfo smallestHammingDistance (const HammingInfo &rHammingInfo1,
-                                        const HammingInfo &rHammingInfo2);
+HammingInfo smallestHammingDistance (const HammingInfo &rsHammingInfo1,
+                                        const HammingInfo &rsHammingInfo2);
 void displayHammingResults (const string &rDNAString, 
                             const HammingInfo &rsHammingInfo);
 
